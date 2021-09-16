482c9f8970f8d95da28eaf0770ead37b1be99a40
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:45:18 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,12 @@
+# wapp to read two integers and swap their values
+
+n1= int(input("enter first integer"))
+n2= int(input("enter second integer"))
+
+print("n1 = ",n1, "n2= ", n2)
+
+n3 = n1
+n1 = n2
+n2 = n3
+
+print("n1 =", n1, "n2 = ", n2)
